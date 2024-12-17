53340e7d804930acc75af2d8d2ac56d993989eb6
Vutha Soy <V010utha@gmail.com>
2024-12-17 16:54:32 +0700

    continue server

--- ChapterII/Learn/server.js
+++ ChapterII/Learn/server.js
@@ -1,11 +1,51 @@
-import http from 'http';
+import http, { get } from 'http';
 const PORT =process.env.PORT; // the port value doenst matter , it's only local host
-const server =http.createServer((req,res)=>{
-    res.write("Hello Duck");
-    
-    
-    res.end();
+import fs from 'fs/promises';
+import path from 'path';
+import { fileURLToPath } from 'url';
+const __filename=fileURLToPath(import.meta.url);
+const __dirname=path.dirname(__filename);
+console.log(__filename,__dirname);
+
+const server =http.createServer(async(req,res)=>{
+    try{
+
+        if(req.method==='GET'){
+            let filepath;
+            if(req.url==='/'){
+                filepath=path.join(__dirname,'Resource','Index.html');
+                res.end("This is the Index");
+
+                
+            }
+            else if(req.url==="/about"){
+                filepath=path.join(__dirname,'Resource',"About.html");
+                
+            }
+            else {
+                res.writeHead(404,{'Content-Type':'text/plain'});
+
+                res.end('404 Not Found\n');
+            }
+
+            const data=await fs.readFile(filepath);
+            res.setHeader('Content-Type','text/html');
+            res.write(data);
+            res.end();
+        }
+        else{
+            res.writeHead(404,{'Content-Type':'text/plain'});
+            res.end('404 Method Not Allowed\n');
+        }
+    }
+    catch(error){
+        res.writeHead(500,{'Content-Type':'text/plain'});
+        res.end('500 Not Found\n');
+    }
+
 })
+
+
 server.listen(PORT,()=>{
     console.log(`Server running on port ${PORT}`);
 })
