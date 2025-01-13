42157b605accc806f7bff5e2e9f339cbc34c566a
Vutha Soy <V010utha@gmail.com>
2025-01-13 20:40:48 +0700

    continue

---
+++ nextjsphrase/src/app/users/[id]/page.tsx
@@ -0,0 +1,8 @@
+
+const page = () => {
+  return (
+    <div>This is the page from the id</div>
+  )
+}
+
+export default page
---
+++ nextjsphrase/src/app/users/page.jsx
@@ -0,0 +1,8 @@
+
+const page = () => {
+  return (
+    <div>HEllo world </div>
+  )
+}
+
+export default page
